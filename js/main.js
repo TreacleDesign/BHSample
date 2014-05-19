2ffef1efbf96f1806f264b87b6662651bd064a9f
TreacleDesign <treacleweb@live.co.uk>
2014-05-19 01:17:50 +0100

    Home And Product Page
    Responsive pages create with HTML5, CSS3, SCSS & Jquery.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-
+$(document).ready(function() {
+	var pull        = $('#nav-menu');  
+        menu        = $('.nav');  
+        menuHeight  = menu.height();  
+  
+    $(pull).on('click', function(e) {  
+        e.preventDefault();  
+        menu.slideToggle();  
+    });
+	
+	$(window).resize(function(){  
+    var w = $(window).width();  
+    if(w > 320 && menu.is(':hidden')) {  
+        menu.removeAttr('style');  
+    }  
+}); 
+});
